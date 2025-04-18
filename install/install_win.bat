83f5cb69e63c40785f508b2380f294eae44fe12e
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-04-19 05:33:56 +0900

    bugfix mkdir cmd

--- install/install_win.bat
+++ install/install_win.bat
@@ -1,13 +1,18 @@
 @echo off
 setlocal
 
-"SCRIPT_DIR"="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set "SCRIPT=%~dp0.."
 set "INSTALL_DIR=%SCRIPT%\bin"
 set "MODELS_DIR=%SCRIPT%\models"
 
-mkdir -p "%INSTALL_DIR%"
-mkdir -p "%MODELS_DIR%"
+if not exist "%INSTALL_DIR%" (
+    echo Creating directory: %INSTALL_DIR%
+    mkdir "%INSTALL_DIR%"
+)
+if not exist "%MODELS_DIR%" (
+    echo Creating directory: %MODELS_DIR%
+    mkdir "%MODELS_DIR%"
+)
 
 curl -o "%INSTALL_DIR%\deep-filter-0.5.6-x86_64-pc-windows-msvc.exe" https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-x86_64-pc-windows-msvc.exe
 
