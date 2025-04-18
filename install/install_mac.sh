32cb2fb8083385a1c740e2d6aa2aaec6053e41bd
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-04-19 05:49:32 +0900

    bugfix curl cmd

--- install/install_mac.sh
+++ install/install_mac.sh
@@ -7,19 +7,20 @@ MODELS_DIR="$SCRIPT_DIR/../models"
 mkdir -p "$BIN_DIR"
 mkdir -p "$MODELS_DIR"
 
-curl -o "$BIN_DIR/deep-filter-0.5.6-aarch64-apple-darwin" https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-aarch64-apple-darwin
+curl -L -o "$BIN_DIR/deep-filter-0.5.6-aarch64-apple-darwin" https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-aarch64-apple-darwin
 chmod +x "$BIN_DIR/deep-filter-0.5.6-aarch64-apple-darwin"
 
-curl -o "$MODELS_DIR/DeepFilterNet2_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx.tar.gz
-curl -o "$MODELS_DIR/DeepFilterNet2_onnx_ll.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx_ll.tar.gz
-curl -o "$MODELS_DIR/DeepFilterNet3_ll_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_ll_onnx.tar.gz
-curl -o "$MODELS_DIR/DeepFilterNet3_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_onnx.tar.gz
+curl -o "$MODELS_DIR/DeepFilterNet2_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx.tar.gz
+curl -o "$MODELS_DIR/DeepFilterNet2_onnx_ll.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx_ll.tar.gz
+curl -o "$MODELS_DIR/DeepFilterNet3_ll_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_ll_onnx.tar.gz
+curl -o "$MODELS_DIR/DeepFilterNet3_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_onnx.tar.gz
 
 # https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-aarch64-apple-darwin
 # https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-armv7-unknown-linux-gnueabihf
 # https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-x86_64-pc-windows-msvc.exe
 
-# https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx.tar.gz
-# https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx_ll.tar.gz
-# https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_ll_onnx.tar.gz
-# https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_onnx.tar.gz
+# https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx.tar.gz
+# https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx_ll.tar.gz
+# https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_ll_onnx.tar.gz
+# https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_onnx.tar.gz
+
--- install/install_win.bat
+++ install/install_win.bat
@@ -14,18 +14,18 @@ if not exist "%MODELS_DIR%" (
     mkdir "%MODELS_DIR%"
 )
 
-curl -o "%INSTALL_DIR%\deep-filter-0.5.6-x86_64-pc-windows-msvc.exe" https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-x86_64-pc-windows-msvc.exe
+curl -L -o "%INSTALL_DIR%\deep-filter-0.5.6-x86_64-pc-windows-msvc.exe" https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-x86_64-pc-windows-msvc.exe
 
-curl -o "%MODELS_DIR%\DeepFilterNet2_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx.tar.gz
-curl -o "%MODELS_DIR%\DeepFilterNet2_onnx_ll.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx_ll.tar.gz
-curl -o "%MODELS_DIR%\DeepFilterNet3_ll_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_ll_onnx.tar.gz
-curl -o "%MODELS_DIR%\DeepFilterNet3_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_onnx.tar.gz
+curl -o "%MODELS_DIR%\DeepFilterNet2_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx.tar.gz
+curl -o "%MODELS_DIR%\DeepFilterNet2_onnx_ll.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx_ll.tar.gz
+curl -o "%MODELS_DIR%\DeepFilterNet3_ll_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_ll_onnx.tar.gz
+curl -o "%MODELS_DIR%\DeepFilterNet3_onnx.tar.gz" https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_onnx.tar.gz
 
 @REM https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-aarch64-apple-darwin
 @REM https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-armv7-unknown-linux-gnueabihf
 @REM https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/deep-filter-0.5.6-x86_64-pc-windows-msvc.exe
 
-@REM https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx.tar.gz
-@REM https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet2_onnx_ll.tar.gz
-@REM https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_ll_onnx.tar.gz
-@REM https://github.com/Rikorose/DeepFilterNet/blob/main/models/DeepFilterNet3_onnx.tar.gz
+@REM https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx.tar.gz
+@REM https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet2_onnx_ll.tar.gz
+@REM https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_ll_onnx.tar.gz
+@REM https://github.com/Rikorose/DeepFilterNet/raw/refs/heads/main/models/DeepFilterNet3_onnx.tar.gz
